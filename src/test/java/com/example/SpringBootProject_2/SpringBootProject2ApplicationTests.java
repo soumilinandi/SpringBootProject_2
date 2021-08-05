8efc4915a534b11f5ca2baca918effcda79f7c51
Soumili Nandi <soumilinandi99@gmail.com>
2021-08-05 09:46:50 +0530

    initial commit

---
+++ src/test/java/com/example/SpringBootProject_2/SpringBootProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootProject_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
