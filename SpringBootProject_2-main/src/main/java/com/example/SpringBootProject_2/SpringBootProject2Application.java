2343f91232326fef699596e423aef5ba4dd12b03
Shomil Maurya <ani.0399kush@gmail.com>
2021-08-05 16:33:38 +0530

    index_html_inserted

---
+++ SpringBootProject_2-main/src/main/java/com/example/SpringBootProject_2/SpringBootProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootProject_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject2Application.class, args);
+	}
+
+}
